5da92309f390e6157006a91ff46acced1c80f045
laksmanayudha <laksmanayudha22@gmail.com>
2025-12-27 13:35:30 +0800

    first commit

---
+++ fmt.go
@@ -0,0 +1,10 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	firstName := "Laksmana"
+// 	lastName := "Yudha"
+
+// 	fmt.Printf("Hello '%s %s'\n", firstName, lastName)
+// }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module belajar-golang-stdlib
+
+go 1.24.2
