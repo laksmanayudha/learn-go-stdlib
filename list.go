432734a428d39f563fbd9b0d84631755458f4e9f
laksmanayudha <laksmanayudha22@gmail.com>
2026-01-05 22:57:09 +0800

    learn math, strconv, list, ring

---
+++ list.go
@@ -0,0 +1,21 @@
+package main
+
+// import (
+// 	"container/list"
+// 	"fmt"
+// )
+
+// func main() {
+// 	var data *list.List = list.New()
+
+// 	data.PushBack("Laksmana")
+// 	data.PushBack("Yudha")
+// 	data.PushBack("Dede")
+
+// 	var head *list.Element = data.Front()
+// 	fmt.Println(head.Next().Next().Value)
+
+// 	for el := data.Front(); el != nil; el = el.Next() {
+// 		fmt.Println(el.Value)
+// 	}
+// }
--- strconv.go
+++ strconv.go
@@ -1,22 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+// import (
+// 	"fmt"
+// 	"strconv"
+// )
 
-func main() {
-	result, err := strconv.ParseBool("True")
-	if err == nil {
-		fmt.Println("Hasil", result)
-	} else {
-		fmt.Println("Error", err.Error())
-	}
+// func main() {
+// 	result, err := strconv.ParseBool("True")
+// 	if err == nil {
+// 		fmt.Println("Hasil", result)
+// 	} else {
+// 		fmt.Println("Error", err.Error())
+// 	}
 
-	resultInt, err := strconv.Atoi("1000")
-	if err != nil {
-		fmt.Println("Error", err.Error())
-	} else {
-		fmt.Println("Hasil", resultInt)
-	}
-}
+// 	resultInt, err := strconv.Atoi("1000")
+// 	if err != nil {
+// 		fmt.Println("Error", err.Error())
+// 	} else {
+// 		fmt.Println("Hasil", resultInt)
+// 	}
+// }
