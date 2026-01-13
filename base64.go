bc4d740be361c10f5016d8136641f6c9281ec1d7
laksmanayudha <laksmanayudha22@gmail.com>
2026-01-13 22:49:21 +0800

    learn encoding, bufio, path, slices

--- base64.go
+++ base64.go
@@ -1,22 +1,22 @@
 package main
 
-import (
-	"encoding/base64"
-	"fmt"
-)
+// import (
+// 	"encoding/base64"
+// 	"fmt"
+// )
 
-func main() {
-	value := "Laksmana Yudha"
+// func main() {
+// 	value := "Laksmana Yudha"
 
-	encoded := base64.StdEncoding.EncodeToString([]byte(value))
-	fmt.Println(encoded)
+// 	encoded := base64.StdEncoding.EncodeToString([]byte(value))
+// 	fmt.Println(encoded)
 
-	decoded, err := base64.StdEncoding.DecodeString(encoded)
+// 	decoded, err := base64.StdEncoding.DecodeString(encoded)
 
-	if err != nil {
-		fmt.Println("Error", err.Error())
-	} else {
-		fmt.Println(decoded)
-		fmt.Println(string(decoded))
-	}
-}
+// 	if err != nil {
+// 		fmt.Println("Error", err.Error())
+// 	} else {
+// 		fmt.Println(decoded)
+// 		fmt.Println(string(decoded))
+// 	}
+// }
---
+++ csv_writer.go
@@ -0,0 +1,15 @@
+package main
+
+// import (
+// 	"encoding/csv"
+// 	"os"
+// )
+
+// func main() {
+// 	writer := csv.NewWriter(os.Stdout)
+
+// 	_ = writer.Write([]string{"laksmana", "yudha", "dede"})
+// 	_ = writer.Write([]string{"buid", "pratama", "nugraha"})
+
+// 	writer.Flush()
+// }
